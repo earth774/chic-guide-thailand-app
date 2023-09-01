c0af3479d02779d51ae214958b9df09934b79a0b
Chicguide <shdsamui99@gmail.com>
2023-09-02 06:43:52 +0700

    start

---
+++ src/environments/environment.ts
@@ -0,0 +1,29 @@
+export const environment = {
+  production: false,
+  serverUrl: 'https://chic-guide-api-46ue3.ondigitalocean.app/api',
+  appUrl: 'https://trynearme.app',
+  appImageUrl: 'https://trynearme.app/assets/img/nearme.png',
+  appId: 'ChicGuidThailand',
+  fbId: 'YOUR_FB_ID',
+  googleMapsApiKey: 'AIzaSyDYsWCmb62mEqZQ9pNz_QoWPSZHY9GV_s8',
+  androidHeaderColor: '#5588a7',
+  defaultUnit: 'km',
+  defaultLang: 'en',
+  googleClientId: '280692033654-m6308ml8geueqltgcccrat1j7gv44nr8.apps.googleusercontent.com',
+  oneSignal: {
+    appId: 'YOUR_ONESIGNAL_APP_ID',
+  },
+  currency: {
+    code: 'USD',
+    display: 'symbol',
+    digitsInfo: '1.2-2',
+  },
+  firebase :{
+    apiKey: "AIzaSyBP4iMq60Uvl6o0ArPQihaUDcRsfu-k0L4",                             // Auth / General Use
+    appId: "1:280692033654:ios:a4321ed6f538c27862b757",      // General Use
+    projectId: "chic-guide-thailand-59013t",               // General Use
+    authDomain: "chic-guide-thailand-59013t.firebaseapp.com",         // Auth with popup/redirect
+    databaseURL: "https://chic-guide-thailand-59013t.firebaseio.com", // Realtime Database
+    storageBucket: "chic-guide-thailand-59013t.appspot.com",          // Storage
+  }
+};
