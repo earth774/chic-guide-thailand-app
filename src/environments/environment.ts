e51dcf99a36201a6e5faf1e17110f488c19e1bfd
earth774 <sutthiphongnuanma@gmail.com>
2023-09-24 21:17:13 +0700

    try update env of prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,31 +1,32 @@
 export const environment = {
-  production: false,
-  serverUrl: 'https://api.chicguidethailand.com/api',
-  appUrl: 'https://trynearme.app',
-  appImageUrl: 'https://trynearme.app/assets/img/nearme.png',
-  appId: 'ChicGuidThailand',
-  fbId: '264172519827432',
-  googleMapsApiKey: 'AIzaSyDYsWCmb62mEqZQ9pNz_QoWPSZHY9GV_s8',
-  androidHeaderColor: '#5588a7',
-  defaultUnit: 'km',
-  defaultLang: 'en',
-  googleClientId: '824153985838-ng4e3ig620o55bb40j7ak1ooscclhjeb.apps.googleusercontent.com',
-  oneSignal: {
-    appId: '527b4f66-61bc-4c47-a512-a41f5cf3fc17',
-    googleProjectNumber: '1071340130209'
-  },
-  currency: {
-    code: 'USD',
-    display: 'symbol',
-    digitsInfo: '1.2-2',
-  },
-  firebase :{
-    apiKey: "AIzaSyBXZXwRGVjdGoGoYylrIBXemcsZ47e1rLs",
-    authDomain: "chic-guide-thailand2.firebaseapp.com",
-    projectId: "chic-guide-thailand2",
-    storageBucket: "chic-guide-thailand2.appspot.com",
-    messagingSenderId: "427617387136",
-    appId: "1:427617387136:web:33e2c135c3f1360ecb9c65",
-    measurementId: "G-7BV434TDPL"
-  }
-};
+    production: true,
+    serverUrl: 'https://api.chicguidethailand.com/api',
+    appUrl: 'https://trynearme.app',
+    appImageUrl: 'https://trynearme.app/assets/img/nearme.png',
+    appId: 'ChicGuidThailand',
+    fbId: '264172519827432',
+    googleMapsApiKey: 'AIzaSyDYsWCmb62mEqZQ9pNz_QoWPSZHY9GV_s8',
+    androidHeaderColor: '#5588a7',
+    defaultUnit: 'km',
+    defaultLang: 'en',
+    googleClientId: '824153985838-ng4e3ig620o55bb40j7ak1ooscclhjeb.apps.googleusercontent.com',
+    oneSignal: {
+      appId: '527b4f66-61bc-4c47-a512-a41f5cf3fc17',
+      googleProjectNumber: '1071340130209'
+    },
+    currency: {
+      code: 'USD',
+      display: 'symbol',
+      digitsInfo: '1.2-2',
+    },
+    firebase :{
+      apiKey: "AIzaSyBXZXwRGVjdGoGoYylrIBXemcsZ47e1rLs",
+      authDomain: "chic-guide-thailand2.firebaseapp.com",
+      projectId: "chic-guide-thailand2",
+      storageBucket: "chic-guide-thailand2.appspot.com",
+      messagingSenderId: "427617387136",
+      appId: "1:427617387136:web:33e2c135c3f1360ecb9c65",
+      measurementId: "G-7BV434TDPL"
+    }
+  };
+  
